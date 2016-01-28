0885e8e66bd853a036dd54beaa653c4d29f15baa
Sofonov Evgeniy <Sofcom@users.noreply.github.com>
2016-01-28 22:29:35 +0500

    Create se8r01.h

---
+++ se8r01.h
@@ -0,0 +1,22 @@
+#ifndef se8r01_h
+#define se8r01_h
+
+#include "API.h"
+
+
+
+//---------------------------------------------
+#define CEq       8
+// CE_BIT:   Digital Input     Chip Enable Activates RX or TX mode
+#define CSNq      9
+// CSN BIT:  Digital Input     SPI Chip Select
+#define SCKq      10
+// SCK BIT:  Digital Input     SPI Clock
+#define MOSIq     11
+// MOSI BIT: Digital Input     SPI Slave Data Input
+#define MISOq     12
+// MISO BIT: Digital Output    SPI Slave Data Output, with tri-state option
+#define IRQq      13
+// IRQ BIT:  Digital Output    Maskable interrupt pin
+//*********************************************
+#endif
